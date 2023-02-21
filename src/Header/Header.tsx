7c11753fdf8976684468bb98084f22a0610d7c64
Tommy <dfgthii@gmail.com>
2023-02-21 19:17:39 +0300

    feat: (add header style and logic)

---
+++ src/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from "./header.module.scss"
+
+const Header = () => {
+
+    return (
+        <div className={s.header}>
+            <div className={s.nav}>
+                <a className={s.aHref} href="#Main">Main</a>
+                <a className={s.aHref} href="#Skills">Skills</a>
+                <a className={s.aHref} href="#Portfolio">Portfolio</a>
+                <a className={s.aHref} href="#Resume">Resume</a>
+                <a className={s.aHref} href="#Contacts">Contacts</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
