68f92a1a694d7b3bd708ffe3725ff1894cf941f8
Tommy <dfgthii@gmail.com>
2023-02-21 19:18:02 +0300

    feat: (add main style and logic)

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import s from "./Main.module.scss"
+import s from './Main.module.scss'
+import photo from 'assets/img/Main/portpet.jpg'
+import Typewriter from "typewriter-effect";
 
 
 const Main = () => {
     return (
-        <div className={s.main}>
-            <div className={s.containerMain}>
-                <div className={s.textBlock}>
-                    <span>Hi There</span>
-                    <h1>I am Rostislav</h1>
-                    <p>I'm Frontend Developer</p>
-                </div>
-                <div className={s.photoBlock}>
-                    <img src="/src/Main/Me.jpg"/>
+        <div className={s.container} id='Main'>
+            <div className={s.main}>
+                <div className={s.containerMain} >
+                    <div className={s.textBlock}>
+                        <h2>Welcome</h2>
+                        <h1 className={s.h1}>
+                            <Typewriter
+                                options={{
+                                    strings: [
+                                        "I'm Rostislav.",
+                                        "I'm Frontend Developer.",
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                            </h1>
+                        <p className={s.p}>Based in Minsk, Belarus.</p>
+                        <button className={s.btn}>SEND ME</button>
+                    </div>
+                    <div className={s.photoBlock}>
+                        <img className={s.photo} src={photo}/>
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
