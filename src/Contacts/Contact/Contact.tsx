33ba87cadbe46f4ded8781b5a617be817c054b46
Tommy <dfgthii@gmail.com>
2023-02-21 19:13:53 +0300

    feat: (add contacts style and logic)

--- src/Contacts/Contact/Contact.tsx
+++ src/Contacts/Contact/Contact.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import s from "./Contact.module.css"
+import s from "./Contact.module.scss"
 
 type propsType = {
     title: string,
     text: string,
+    icon: any
 }
 
 
 const Contact = (props: propsType) => {
-    return (
-        <div className={s.skill}>
-            <div className={s.icon}></div>
-            <div className={s.description}>
-                <h4 className={s.h4}>{props.title}</h4>
-                <p className={s.p}>{props.text} </p>
+        return (
+            <div className={s.skill}>
+                <div className={s.icon}>
+                    <img src={props.icon} alt={"icon"}/>
+                </div>
+                <div className={s.description}>
+                    <h4 className={s.h4}>{props.title}</h4>
+                    <p className={s.p}>{props.text} </p>
+                </div>
             </div>
-
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default Contact;
--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import s from "./Contacts.module.css"
-import sCommon from "../common/Common.module.css"
-import Contact from "./Contact/Contact";
-import {Title} from "../common/Title/Title";
+import s from './Contacts.module.scss';
+import Contact from './Contact/Contact';
+import {Title} from '../common/Title/Title';
+
+import callIcon from 'assets/img/Contacts/callIcon.png'
+import emailIcon from 'assets/img/Contacts/emailIcon.png'
+import localIcon from 'assets/img/Contacts/localIcon.png'
 
 
 const Contacts = () => {
     return (
-        <div className={s.contactBlock}>
-            <div className={`${sCommon.container} ${s.container}`}>
+        <div className={s.contactBlock} id="Contacts">
+            <div className={s.container}>
                 <Title title={"GET IN TOUCH"}/>
 
                 <div className={s.containerMini}>
                     <div className={s.contactContainer}>
-                        <Contact title="Phone" text="+375292546933"/>
-                        <Contact title="Email" text="dfgthii@gmail.com"/>
-                        <Contact title="Location" text="Minsk, Belarus"/>
+                        <Contact title="Phone" text="+375292546933" icon={callIcon}/>
+                        <Contact title="Email" text="dfgthii@gmail.com" icon={emailIcon}/>
+                        <Contact title="Location" text="Minsk, Belarus" icon={localIcon}/>
                     </div>
+
                     <form className={s.formContainer}>
                         <div className={s.inputContainer}>
                             <input type="text" placeholder="Your Name"/>
@@ -27,7 +31,9 @@ const Contacts = () => {
                         <div className={s.textareaContainer}>
                             <textarea placeholder="Your Message"></textarea>
                         </div>
-                        <div className={s.buttonContainer}> <button>SEND ME</button></div>
+                        <div className={s.buttonContainer}>
+                            <button className={s.btn}>SEND ME</button>
+                        </div>
                     </form>
                 </div>
             </div>
