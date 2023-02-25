519bc9ae2a74cdbb3b88b6008e699c21f857c33e
Tommy <dfgthii@gmail.com>
2023-02-25 19:23:18 +0300

    fix: (add margin for input contact)

--- src/MyPortfolio/MyPortfolio.tsx
+++ src/MyPortfolio/MyPortfolio.tsx
@@ -21,7 +21,7 @@ const MyPortfolio = () => {
                 <Title title={"MY PORTFOLIO"}/>
                 <div className={s.portfolio}>
                     <Portfolio title="Todolist" text="TS, React, Redux, RestAPI, Thunk,Material UI, Unit Tests, Formik" img={imgOne} link={'https://tommysheully.github.io/MeToDo/#/ToDoListsList'}/>
-                    <Portfolio title="THE SOCIAL NETWORK" text="TS, Redux, RestAPI, Thunk,ANT Design, Unit Tests, Formik" img={imgTwo}/>
+                    <Portfolio title="THE SOCIAL NETWORK" text="TS, Redux, RestAPI, Thunk,ANT Design, Unit Tests, Formik" img={imgTwo} link={'https://tommysheully.github.io/social-network/'}/>
                     <Portfolio title="Counter" text="TS, Redux, React" img={imgThree} link={'https://tommysheully.github.io/TestTasksFor1MothStydy/'}/>
                     <Portfolio title="Team Project" text="TS, React, Redux Toolkit, Axios, Material UI, Formik" img={imgFour} link={'https://igor-tech.github.io/friday-project-cards/'}/>
                     <Portfolio title="Portfolio" text="TS, HTML, SCSS, Formik, Typewriter-effect" img={imgFive} link={'https://tommysheully.github.io/Portfolio/'}/>
