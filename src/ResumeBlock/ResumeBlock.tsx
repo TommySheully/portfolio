ce487ee83ea3f14ed373ef1c3c24b9587200c54f
Tommy <dfgthii@gmail.com>
2023-05-20 13:17:01 +0300

    fix: (resume block)

--- src/ResumeBlock/ResumeBlock.tsx
+++ src/ResumeBlock/ResumeBlock.tsx
@@ -12,28 +12,32 @@ export const ResumeBlock = () => {
                     <div className={s.educationContainer}>
                         <h3 className={s.h3}>My Experience</h3>
                         <Experience
-                            dataP={'2022 - 2023'}
+                            dataP={'2022 - PRESENT'}
+                            titleH2={'Frontend Developer'}
+                            titleH3={'PART-TIME'}
+                            text={'Maintenance and minor maintenance of websites and applications. Creation of landing pages and spa applications, support and review of legacy code, transferring the application from class components to functional ones, working with forms, validation, tests, RESTful, BLL, DAL, Bug fixing, CRUD operation and data processing, deploy.'}/>
+                        <Experience
+                            dataP={'2022 - 2022'}
                             titleH2={'Frontend Developer'}
                             titleH3={'IT-Incubator'}
                             text={'Study in an IT-Incubator. Learning React, Redux, JS, TS, html, css and so on.'}/>
                         <Experience
-                            dataP={'2019 - 2023'}
-                            titleH2={'Psychology, Psychologist Practitioner '}
-                            titleH3={'Belarusian State Pedagogical University M. Tanka, Minsk'}
-                            text={'Education at the University of Psychology, Psychologist Practitioner'}/>
-                        <Experience
-                            dataP={'2019 - 2022'}
+                            dataP={'2018 - 2022'}
                             titleH2={'Project Manager'}
                             titleH3={'IE Bychinov E.V'}
-                            text={'Keeping records of the management team. Maintaining daily / weekly / monthly / annual reports. Сommunication with wholesale suppliers and buyers. Financial transactions. Solving current issues and difficulties of the company, analysis-research of the market for company positions.'}/>
-
+                            text={'Team management. Maintaining daily / weekly / monthly / annual reports. Communication with wholesale suppliers and buyers. Financial transactions. Solving current issues and difficulties of the company, analysis-research of the market for company positions.'}/>
                     </div>
                     <div className={s.softSkillsContainer}>
                         <h3 className={s.h3}>Soft Skills</h3>
                         <Experience
                             dataP={'Communication'}
                             titleH2={'Interpersonal Skill'}
-                            text={'The ability to communicate in a team, ask questions, the ability to speak facts, to hear others.'}/>
+                            text={'✔ The ability to communicate in a team\n' +
+                                '✔ Ask questions\n' +
+                                '✔ Hear others\n' +
+                                '✔ Resilience\n' +
+                                '✔ Sense of humor\n' +
+                                '✔ Adaptability\n' }/>
                         <Experience
                             dataP={'Soft skills'}
                             titleH2={'Social Intelligence '}
